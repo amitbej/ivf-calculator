77c5073fa890b844b584522f4b77e58033db44c7
Amit Bej <amitbej0@gmail.com>
2024-11-29 00:22:31 +0530

    added orange box

--- components/CheckboxGroup.js
+++ components/CheckboxGroup.js
@@ -9,16 +9,16 @@ const CheckboxGroup = ({ options, selected, onChange }) => {
   };
 
   return (
-    <div className="flex flex-col md:flex-row items-start gap-0 md:gap-10 mx-4 md:mx-0 py-4">
+    <div className="flex flex-col md:flex-row items-start gap-4 mx-4 md:mx-0 py-4">
       {options.map((option, index) => (
-        <div key={index} className="flex items-center mb-2">
+        <div key={index} className="flex items-center mb-3">
           <input
             type="checkbox"
-            className="mr-2 cursor-pointer"
+            className="mr-3 cursor-pointer w-5 h-5 rounded-sm border-2 border-gray-400 checked:border-orange-500 checked:bg-orange-500 focus:ring-2 focus:ring-orange-500 appearance-none"
             checked={selected.includes(option)}
             onChange={() => handleCheckboxChange(option)}
           />
-          <span className="text-lg">{option}</span>
+          <span className="text-lg text-gray-700">{option}</span>
         </div>
       ))}
     </div>
