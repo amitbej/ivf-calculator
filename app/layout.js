002e84ce4bb3b30c0af5dd4fe690a39fd7edd369
Amit Bej <amitbej0@gmail.com>
2024-11-29 01:10:06 +0530

    layoutpage

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,30 @@
 import { NavBar } from "@/components/Navbar";
+import localFont from "next/font/local";
 import "./globals.css";
 
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata = {
+  title: "IVF Calculator",
+  description: "Made By Amit Bej",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html>
-      <head>
-        <title>IVF Success Calculator</title>
-      </head>
-      <body>
+      <body >
         <header>
-          <nav >
-            <NavBar/>
+          <nav>
+            <NavBar />
           </nav>
         </header>
         <main>{children}</main>
